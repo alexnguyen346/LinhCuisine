971b126c93126f526bd83d0e4f2583d65d015fce
alexnguyen346 <37456127+alexnguyen346@users.noreply.github.com>
2022-09-10 19:58:11 -0400

    First Page Rough

--- script.js
+++ script.js
@@ -13,3 +13,15 @@ if(close){
 		nav.classList.remove('active')
 	})
 }
+
+let carts = document.querySelectorAll('.add-cart');
+
+for(let i=0; i < carts.length; i++){
+	carts[i].addEventListener('click', ()=> {
+		cartNumbers();
+	})
+}
+
+function cartNumbers(){
+	localStorage.setItem('cartNumbers', 1);
+}
